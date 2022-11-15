82173734a6ec9970485c7ad013c34164034a952c
Sara-0x53617261 <sara98.sain@gmail.com>
2022-11-15 04:49:25 -0800

    DLL hack progress

--- DLLMemoryHack/src/lib.rs
+++ DLLMemoryHack/src/lib.rs
@@ -1,4 +1,16 @@
-#[ctor::ctor]
-fn ctor() {
-    println!("Hello from dll");
+use user32::MessageBoxA;
+use winapi::um::winuser::{MB_OK, MB_ICONINFORMATION};
+use std::ffi::CString;
+
+#[no_mangle]
+extern "system" fn DllMain(_: *const u8, _: u32, _: *const u8 ) -> u32 {
+    unsafe {
+        MessageBoxA(
+            std::ptr::null_mut(),
+            CString::new("Hello from the .dll").unwrap().as_ptr(),
+            CString::new("hello.dll").unwrap().as_ptr(),
+            MB_OK | MB_ICONINFORMATION,
+        );
+    }
+    1 
 }
--- DLLMemoryHack/src/main.rs
+++ DLLMemoryHack/src/main.rs
@@ -1,12 +1,3 @@
-use dll_syringe::{Syringe, process::OwnedProcess};
-
-
 fn main() {
-    let proc_name = "wesnoth.exe";
-
-    let target_proc = OwnedProcess::find_first_by_name(proc_name).unwrap();
-    let injector = Syringe::for_process(target_proc);
-
-    let payload = injector.inject("./target/release/dll_memory_hack.dll").unwrap();
-
+    println!("Hello, world!");
 }
