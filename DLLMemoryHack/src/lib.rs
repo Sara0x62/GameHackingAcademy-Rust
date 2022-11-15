36e6188896ecee9cd189069414937a71ced9bea5
Sara-0x53617261 <sara98.sain@gmail.com>
2022-11-15 07:54:43 -0800

    DLL hack working (using CE to inject)

--- DLLMemoryHack/src/lib.rs
+++ DLLMemoryHack/src/lib.rs
@@ -1,16 +1,51 @@
-use user32::MessageBoxA;
-use winapi::um::winuser::{MB_OK, MB_ICONINFORMATION};
-use std::ffi::CString;
+use winapi::{
+        um::{
+        winuser::{MB_OK, MB_ICONINFORMATION, GetAsyncKeyState}, 
+        winnt::DLL_PROCESS_ATTACH,
+        processthreadsapi::CreateThread,
+        minwinbase::{LPTHREAD_START_ROUTINE, SECURITY_ATTRIBUTES},
+        
+    }, 
+    shared::basetsd::DWORD_PTR
+};
+use winapi::shared::ntdef::NULL;
+use winapi::shared::minwindef::LPVOID;
+
+// used for testing if injector worked
+// use user32::MessageBoxA;
+// use std::ffi::CString;
+
+fn injected_thread() {
+    loop {
+        unsafe {
+            if GetAsyncKeyState('M' as i32) != 0 {
+                let base_addr = 0x017EED18 as *mut DWORD_PTR;
+                let game_base = (*base_addr + 0xA90) as *mut DWORD_PTR;
+                let gold_addr = (*game_base + 0x4) as *mut DWORD_PTR;
+
+                *gold_addr = 9999;
+            }
+        }
+    }
+}
 
 #[no_mangle]
-extern "system" fn DllMain(_: *const u8, _: u32, _: *const u8 ) -> u32 {
+extern "system" fn DllMain(_: *const u8, fwdReason: u32, _: *const u8 ) -> u32 {
     unsafe {
+        if fwdReason == DLL_PROCESS_ATTACH {
+            let lpthread_start = Some(*(&injected_thread() as *const _ as *const unsafe extern "system" fn(LPVOID) -> u32));
+            CreateThread(0 as *mut SECURITY_ATTRIBUTES, 0, lpthread_start, NULL, 0, 0 as *mut u32);
+        }
+        /*
+        // Uncomment to test if it's actually running
         MessageBoxA(
             std::ptr::null_mut(),
             CString::new("Hello from the .dll").unwrap().as_ptr(),
             CString::new("hello.dll").unwrap().as_ptr(),
             MB_OK | MB_ICONINFORMATION,
         );
+        */
     }
+
     1 
 }
--- DLLMemoryHack/src/main.rs
+++ DLLMemoryHack/src/main.rs
@@ -1,3 +1,26 @@
+use inject_lib::Injector;
+use inject_lib::Inject;
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    let get_pid = Injector::find_pid(inject_lib::Data::Str("wesnoth.exe")).unwrap();
+
+    println!("get_pid: {:?}", get_pid);
+
+    let path = Path::new("C:\\Users\\IEUser\\Desktop\\GameHacking-Academy\\DLLMemoryHack\\target\\i686-pc-windows-msvc\\debug\\dll_memhack.dll");
+    let dll_file = inject_lib::Data::Path(path);
+    
+    println!("DLL PATH: {:?}", dll_file);
+
+    let pid = get_pid.into_iter().nth(0).unwrap();
+
+    println!("PID: {}", pid);
+
+    // TODO - Fix injector - currently crashes the game.
+    let inject = Injector::new(dll_file, pid);
+    let res = inject.inject(false);
+    let result = res.inject();
+    println!("Res: {:?}", result);
+
+    println!("Injected!");
 }
